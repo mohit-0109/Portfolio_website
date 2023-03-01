8379ae15e40c4b81a45974498990b22181de4157
mohit-0109 <mohit.nandargi@gmail.com>
2023-03-01 22:31:40 +0530

    initial-contact-description

--- portfolio-web/pages/contact.js
+++ portfolio-web/pages/contact.js
@@ -3,6 +3,10 @@ export default function contact()
     return(
         <div>
             This is the contacts page.
+            <a>Linkeidn</a>
+            <a>Github</a>
+            <a>Twitter</a>
+            <a>Gmail</a>
         </div>
     )
 }
